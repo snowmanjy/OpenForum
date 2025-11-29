4c6e2e1556186e628abaeb988ef471c0bae90fba
Yang Jing <yang@Yangs-Mac-mini.local>
2025-11-29 15:40:29 -0800

    added kafka config for docker

--- forum-boot/src/main/java/com/openforum/boot/OpenForumApplication.java
+++ forum-boot/src/main/java/com/openforum/boot/OpenForumApplication.java
@@ -2,8 +2,12 @@ package com.openforum.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.openforum")
+@EnableJpaRepositories(basePackages = "com.openforum.infra.jpa.repository")
+@EntityScan(basePackages = "com.openforum.infra.jpa.entity")
 public class OpenForumApplication {
 
     public static void main(String[] args) {
